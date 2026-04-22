38643f36af3023ec47185a84457b7420d3b0e5b1
TeamSwordphin <jackie_yeung@protonmail.com>
2026-04-22 09:50:00 -0400

    ASteroids

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import pygame
-from logger import log_state
+import sys
+
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+
+    dt = 0
 
     while True:
         log_state()
@@ -18,7 +38,20 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collide_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
+
